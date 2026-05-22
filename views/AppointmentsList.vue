b8421b42b85601f3e8b0a161e24264141c45f373
Ibrahim Marwan Assaf <ibrahim_assaf@icloud.com>
2026-05-22 04:50:02 +0300

    Update API_BASE_URL to include '/prod' path

--- views/AppointmentsList.vue
+++ views/AppointmentsList.vue
@@ -58,7 +58,7 @@
 </template>
 
 <script>
-const API_BASE_URL = "https://kd5k8ocfs3.execute-api.eu-central-1.amazonaws.com";
+const API_BASE_URL = "https://kd5k8ocfs3.execute-api.eu-central-1.amazonaws.com/prod";
 
 export default {
   name: "AppointmentsList",
